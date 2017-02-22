2d77b0dca40db56c8420e24e89021f190d6c9e01
yaryn <yaryn@users.noreply.github.com>
2017-02-22 17:49:11 +0000

    20170222T174800Z

---
+++ initials.c
@@ -0,0 +1,29 @@
+#include<cs50.h>
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+int print_initials(string s);
+
+int main (void)
+{
+    string s = GetString();
+    
+    if (s != NULL)
+    {
+        print_initials(s);
+    }
+    printf("\n");
+}
+
+int print_initials(string s)
+{
+    printf("%c", toupper(s[0]));
+        
+        for (int i = 1, l = strlen(s); i < l; i++)
+        {
+            if (s[i-1] == ' ') 
+                printf("%c", toupper(s[i]));
+        }
+    return 0;
+}
